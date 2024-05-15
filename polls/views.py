6f93e91ee88b572fa23e30ecc4cb288945e08ab3
candeperles <candeperles@hotmail.com>
2024-05-15 12:04:36 -0300

    Initial commit

---
+++ polls/views.py
@@ -0,0 +1,15 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from polls.models import Question
+from django.template import loader 
+
+# Create your views here.
+def index (request):
+    questions = Question.objects.order_by("-date")
+    context ={
+        "questions":questions
+    }
+    return render(request, "index.html", context)
+
+def detail(request, question_id):
+    return HttpResponse("This is question number: " + str(question_id))
